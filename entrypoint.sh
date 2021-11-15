7d8d0455912639ff7dfc417f24911e1f9366c919
Helix <hissyu@hotmail.com>
2021-11-15 13:42:45 +0000

    small improve when using algolia

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,24 +16,26 @@ git config --global user.email "$INPUT_USER_EMAIL"
 npm install hexo-cli -g
 npm install hexo-deployer-git --save
 
+hexo_params="g -d"
 # algolia
-if ["$USE_ALGOLIA" = true]
+if [ "$USE_ALGOLIA" = true ]
 then
+    hexo_params="d"
     hexo a
 fi
 
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
 then
-    hexo g -d
+    hexo $hexo_params
 elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
 then
     # pull original publish repo
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d
+    hexo $hexo_params
 else
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d -m "$INPUT_COMMIT_MSG"
+    hexo $hexo_params -m "$INPUT_COMMIT_MSG"
 fi
 
 echo ::set-output name=notify::"Deploy complate."
